6786c7ab1bc3d8154970343389a5bac3b9ab269e
Jonathan Paiva <jonathan.paiva.m@hotmail.com>
2021-09-10 19:19:08 -0300

    Atualizações sobre definições de tipos, variáveis, decisões

---
+++ IntroducaoPHP/9-operador-ternario.php
@@ -0,0 +1,11 @@
+<?php
+
+$idade = 15;
+
+/*
+Temos uma outra opção para tomar decisões. Se precisamos, por exemplo, atribuir o valor a uma variável nos baseando em alguma condição, podemos utilizar o que é chamado de OPERADOR TERNÁRIO. Sua sintaxe é a seguinte:
+*/
+
+$mensagem = $idade < 18 ? 'Você é menor de idade' : 'Você é maior de idade';
+
+echo $mensagem;
