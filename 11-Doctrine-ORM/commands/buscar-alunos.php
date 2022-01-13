481a55c4bb266328966f9806346543a4a01d8783
Jonathan Paiva <jonathan.paiva.m@hotmail.com>
2022-01-13 18:52:13 -0300

    Utilizado o doctrine para fazer consultas/adicições no banco de dados, sem o uso de instruções SQL, apenas código, baseado nos métodos disponível pelo repositório.

---
+++ 11-Doctrine-ORM/commands/buscar-alunos.php
@@ -0,0 +1,55 @@
+<?php
+
+use Alura\Doctrine\Entity\Aluno;
+use Alura\Doctrine\Helper\EntityManagerFactory;
+use Doctrine\DBAL\Schema\ForeignKeyConstraint;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$entityManagerFactory = new EntityManagerFactory() ;
+$entityManager = $entityManagerFactory->getEntityManager();
+
+$alunoRespository = $entityManager->getRepository(Aluno::class);
+
+//Necessário indicar com o $alunoList é um array de alunos com a Annotations, assim podendo utilizar os métodos configurados na classe.
+/**
+ * @var Aluno[] $alunoList
+ */
+$alunoList = $alunoRespository->findAll();
+//Método 'findAll trás do banco de dados conectado todos os registros dessa entidade pesquisada
+
+echo "\n Utilizando a captura da lista com todos os registros, baseado no FindAll:\n";
+foreach ($alunoList as $aluno){
+    echo "ID: {$aluno->getId()} -> Nome: {$aluno->getNome()}";
+}
+echo "\n----------------------------------\n\n";
+
+
+echo "Filtrando um Registro baseado no Find:\n";
+/* Método 'find' pesquisa um objeto específico baseado no ID */
+$filtro1 = $alunoRespository->find(3);
+echo $filtro1->getNome();
+echo "\n----------------------------------\n\n";
+
+
+echo "Filtrando um Array de Registros baseado nos parâmetros informados com o FindBy:\n";
+/* Método 'findBy' pode ser feito vários filtros nas buscas, baseados nos campos da entidade
+O Retorno dessa expressão seria um array baseado nos filtros repassados. */
+$filtro2 = $alunoRespository->findBy([
+    "nome" => "Luiz"
+]);
+
+var_dump($filtro2);
+echo "\n----------------------------------\n\n";
+
+
+echo "Filtrando um Objeto dos Registors baseado nos parâmetros informados com o FindOneBy:\n";
+/*método 'findOneBy' pode ser feito vários filtros nas buscas, baseados nos campos da entidade, trazendo apenas 
+Retornando apenas um objeto com a informação filtrada.*/
+$filtro3= $alunoRespository->findOneBy([
+    "nome" => "Zezin"
+]);
+
+var_dump($filtro3);
+echo "\n----------------------------------\n\n";
+
--- 11-Doctrine-ORM/commands/criar-aluno.php
+++ 11-Doctrine-ORM/commands/criar-aluno.php
@@ -6,15 +6,19 @@ use Alura\Doctrine\Helper\EntityManagerFactory;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $aluno = new Aluno();
-$aluno->setNome("Aluno Teste");
+
+//o uso da expressão $argv[1] significa que será passado o parâmetro via linha de comando, assim podendo ser feito as inclusões de forma mais prática. 
+$aluno->setNome($argv[1]);
+//$aluno->setNome("Aluno Teste");
 
 $entityManagerFactory = new EntityManagerFactory() ;
 $entityManager = $entityManagerFactory->getEntityManager();
 
 $entityManager->persist($aluno);
 
+/*
 $aluno->setNome("Teste");
 
-//método 'flush' serve para efetuar a alteração/inclusão/remoções no banco de dados. 
+//método 'flush' serve para efetuar a alteração/inclusão/remoções no banco de dados. */
 $entityManager->flush();
 
